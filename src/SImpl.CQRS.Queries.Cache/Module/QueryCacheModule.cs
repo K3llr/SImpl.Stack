866c4182f571076a9c90d6504bad43be75a78540
Adrian Ochmann <adrian.ochmann@outlook.com>
2022-10-01 22:54:09 +0200

    fix: updated query cache module registration order

--- src/SImpl.CQRS.Queries.Cache/Module/QueryCacheModule.cs
+++ src/SImpl.CQRS.Queries.Cache/Module/QueryCacheModule.cs
@@ -3,12 +3,13 @@ using SImpl.Cache;
 using SImpl.Cache.Module;
 using SImpl.CQRS.Queries.Cache.Decorators;
 using SImpl.CQRS.Queries.Cache.Services;
+using SImpl.CQRS.Queries.Module;
 using SImpl.Host.Builders;
 using SImpl.Modules;
 
 namespace SImpl.CQRS.Queries.Cache.Module
 {
-    [DependsOn(typeof(CacheModule))]
+    [DependsOn(typeof(CacheModule), typeof(QueryModule))]
     public class QueryCacheModule : IHostBuilderConfigureModule, IServicesCollectionConfigureModule, ICacheExtensionModule
     {
         public QueryCacheModuleConfig Config { get; }
