cecf1db003d3b442928f071cfcda0f0f84d88dc6
Ihor Leleka <ile@novicell.co.uk>
2025-03-31 16:17:14 +0100

    feat: serialize cqrs entity as part of otel activity report

--- src/SImpl.CQRS.OpenTelemetry/Services/Queries/QueryHandlerInstrumentationDecorator.cs
+++ src/SImpl.CQRS.OpenTelemetry/Services/Queries/QueryHandlerInstrumentationDecorator.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using SImpl.CQRS.OpenTelemetry.Helpers;
 using SImpl.CQRS.Queries;
 
 namespace SImpl.CQRS.OpenTelemetry.Services.Queries;
@@ -11,6 +12,7 @@ public class QueryHandlerInstrumentationDecorator<TQuery, TResult>(IQueryHandler
         var type = query.GetType();
         using var activity = Constants.ActivitySource.StartActivity($"HandleAsync: {type.Name}");
         activity?.SetTag("query.fullname", type.FullName);
+        activity?.SetTag("query.serialized", SerializationHelper.SerializeActivityObject(query));
         return await @base.HandleAsync(query);
     }
 }
